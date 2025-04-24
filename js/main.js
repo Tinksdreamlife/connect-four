9075620432c22d1f8541c773ab4a99a610a59a8a
Tinksdreamlife <tinksdreamlife@gmail.com>
2025-04-24 14:36:01 -0400

    Rendered Board

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 /*----- constants -----*/
-
+//look-up data structure
+const COLORS = {
+    '1': 'purple', 
+    '-1': 'pink', 
+    'null': 'white', 
+}
 
   /*----- state variables -----*/
 //Define, but do not assign to (initialize)
@@ -15,10 +20,41 @@ let turn; // 1/-1 -> the player whose turn it is
 
   /*----- functions -----*/
 
+  init();
+
   //The init's function's purpose is to initialize all state, then call render()
   function init() {
-
+    //To visualize the mapping (connection) between
+    //the board arrary and the "cells"/divs in the DOM,
+    //"rotate" the board 90 degrees counterclockwise
+    board = [
+        [null, null, null, null, null, null], //column 0
+        [null, null, null, null, null, null], //column 1
+        [null, null, null, null, null, null], //column 2
+        [null, null, null, null, null, null], //column 3
+        [null, null, null, null, null, null], //column 4
+        [null, null, null, null, null, null], //column 5
+        [null, null, null, null, null, null], //column 6
+    ];
     winner = null;
     turn = 1;
-    // render();
+    render();
+  }
+
+  //The purpose of the render() function is to "transfer"/visualize
+  //ALL state to the DOM
+
+  function render(){
+    renderBoard();
+    // renderMessage();
+    // renderControls();
+  }
+
+  function renderBoard() {
+    board.forEach((colArr, colIdx) => {
+        colArr.forEach((cellVal, rowIdx) => {
+            const cellEl = document.getElementById(`c${colIdx}r${rowIdx}`);
+            cellEl.style.backgroundColor = COLORS[cellVal];
+        });
+    })
   }
